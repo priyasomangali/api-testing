d5dc77c688fb6f4193441cb6eed5146541f6ed0e
priyasr <priyadharsini.sr93@gmail.com>
2019-09-18 11:35:19 +0530

    Schema validation related files added. Support for Extent reports included

--- src/main/java/com/freenow/helpers/CommonUtils.java
+++ src/main/java/com/freenow/helpers/CommonUtils.java
@@ -1,5 +1,36 @@
 package com.freenow.helpers;
 
+import java.util.Properties;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
 public class CommonUtils {
+	static ExtentHtmlReporter htmlReporter;
+	static ExtentReports extent = new ExtentReports();;
+	ExtentTest test;
+	Properties properties;
+
+	public static ExtentReports startReport(String reportName) {
+
+		htmlReporter = new ExtentHtmlReporter("test-output/" + reportName + ".html");
+		extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
+
+		extent.setSystemInfo("OS", "Mac Sierra");
+		extent.setSystemInfo("Host Name", "local");
+		extent.setSystemInfo("Environment", "mac");
+		extent.setSystemInfo("User Name", "user");
+
+		htmlReporter.config().setChartVisibilityOnOpen(true);
+		htmlReporter.config().setDocumentTitle("AutomationTesting.in Demo Report");
+		htmlReporter.config().setReportName(reportName);
+		htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+		htmlReporter.config().setTheme(Theme.DARK);
+		return extent;
+	}
 
 }
--- src/test/java/com/freenow/userblog/tests/SchemaTest.java
+++ src/test/java/com/freenow/userblog/tests/SchemaTest.java
@@ -1,5 +1,62 @@
 package com.freenow.userblog.tests;
 
-public class SchemaTest {
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
+import com.freenow.helpers.CommonUtils;
+import com.freenow.userblog.Endpoints;
+import io.restassured.RestAssured;
+
+public class SchemaTest extends CommonUtils {
+
+	ExtentReports extent;
+	ExtentTest test;
+	Endpoints endpoint = new Endpoints();
+
+	@BeforeClass
+	public void startReports() {
+
+		String reportName = this.getClass().getSimpleName();
+		extent = startReport(reportName);
+		test = extent.createTest(reportName, "Test Summary");
+	}
+
+	@Test
+	public void testJsonUsersSchema() {
+
+		RestAssured.given().when().get(endpoint.generateUrl(Endpoints.users)).then().assertThat()
+				.body(matchesJsonSchemaInClasspath("usersSchema.json/"));
+		test.log(Status.PASS, "Users schema validation successful");
+
+	}
+
+	@Test
+	public void testJsonPostsSchema() {
+
+		RestAssured.given().when().get(endpoint.generateUrl(Endpoints.postsWithUserId)).then().assertThat()
+				.body(matchesJsonSchemaInClasspath("postsSchema.json/"));
+		test.log(Status.PASS, "Posts schema validation successful");
+
+	}
+
+	@Test
+	public void testJsonCommentsSchema() {
+
+		RestAssured.given().when().get(endpoint.generateUrl(Endpoints.commentsWithPostId)).then().assertThat()
+				.body(matchesJsonSchemaInClasspath("commentsSchema.json/"));
+		test.log(Status.PASS, "Comments schema validation successful");
+
+	}
+
+	@AfterClass
+	public void tearDown() {
+		extent.flush();
+	}
 }
