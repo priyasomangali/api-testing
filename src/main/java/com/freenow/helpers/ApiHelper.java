5ef276edd07124c07c0b78daf732407178149984
priyasr <priyadharsini.sr93@gmail.com>
2019-09-18 10:17:09 +0530

    Dependencies , Api methods and endpoints updated

--- src/main/java/com/freenow/helpers/ApiHelper.java
+++ src/main/java/com/freenow/helpers/ApiHelper.java
@@ -1,5 +1,38 @@
 package com.freenow.helpers;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
+
+import java.util.Map;
+
+// This class has all the REST API method definitions
+
 public class ApiHelper {
 
+	public Response getRequest(String endpoint) {
+		return given().headers("Content-Type", ContentType.JSON).when().get(endpoint).then()
+				.contentType(ContentType.JSON).extract().response();
+	}
+
+	public Response getRequest(String endpoint, Map<String, Object> queryParam) {
+		return given().headers("Content-Type", ContentType.JSON).queryParams(queryParam).when().get(endpoint).then()
+				.contentType(ContentType.JSON).extract().response();
+	}
+
+	public Response postRequest(String endpoint, String requestBody) {
+		return given().contentType(ContentType.JSON).body(requestBody).post(endpoint);
+	}
+
+	public Response deleteRequest(String endpoint) {
+		return given().contentType(ContentType.JSON).delete(endpoint);
+	}
+
+	public Response putRequest(String endpoint, String requestBody) {
+		return given().contentType(ContentType.JSON).body(requestBody).put(endpoint);
+	}
+
+	public Response patchRequest(String endpoint, String requestBody) {
+		return given().contentType(ContentType.JSON).body(requestBody).patch(endpoint);
+	}
 }
--- src/main/java/com/freenow/userblog/Endpoints.java
+++ src/main/java/com/freenow/userblog/Endpoints.java
@@ -1,5 +1,16 @@
 package com.freenow.userblog;
 
 public class Endpoints {
+	private static String baseUrl = "https://jsonplaceholder.typicode.com";
+	public static String users = "/users";
+	public static String posts = "/posts";
+	public static String comments = "/comments";
+	public static String commentsWithPostId = "/comments?postId=";
+	public static String postsWithUserId = "/posts?userId=";
 
+	public String generateUrl(String api) {
+
+		String url = baseUrl + api;
+		return url;
+	}
 }
